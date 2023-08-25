be3558d214c75903cc0683de44572a11cb20c172
MW <matthewrhyswilliams@live.com>
2023-08-25 23:03:40 +0100

    initialised map

--- map.js
+++ map.js
@@ -3,3 +3,10 @@ import designations from './designations-geo.json' assert {type: 'json'};
 
 console.log(boundary);
 console.log(designations);
+
+const map = L.map('map').setView([55.948612, -3.200833], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
