e6ec24029f36ee12f79584599d3b5c48bc4e5dd8
MW <matthewrhyswilliams@live.com>
2023-08-25 22:52:32 +0100

    map css file created

---
+++ map.js
@@ -0,0 +1,5 @@
+import boundary from './boundary-geo.json' assert {type: 'json'};
+import designations from './designations-geo.json' assert {type: 'json'};
+
+console.log(boundary);
+console.log(designations);
