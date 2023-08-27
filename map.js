b26f4ecb00f429faa87c85e344c3ba20d5cfb9e7
MW <matthewrhyswilliams@live.com>
2023-08-27 22:24:11 +0100

    colours object created, layer control created and listed buildings toggling

--- map.js
+++ map.js
@@ -23,39 +23,49 @@ function setupMap() {
 
 function getLayers() {
 
+  const layers = {};
   const listedGroup = new L.LayerGroup();
+  const colours = {
+    listed: '#000000',
+    consarea: '#FF0000',
+    garden: '#00FF00',
+    monument: '#FFFF00',
+    world: '#0000FF',
+  };
 
   for (const des of castle.designations) {
 
-      const popup = `
-        ${des.type}
-        <br>
-        <a target="_blank" href="${des.url}">${des.title}</a>
-        <br>
-        ${des.number}
-      `;
+    const popup = `
+      ${des.type}
+      <br>
+      <a target="_blank" href="${des.url}">${des.title}</a>
+      <br>
+      ${des.number}
+    `;
 
     switch (des.type) {
 
       case 'Listed Building':
-        const listed = createNewLayer(des.geometry, '#000000', popup, 'Point');
+        const listed = createNewLayer(des.geometry, colours.listed, popup, 'Point');
         listedGroup.addLayer(listed).addTo(map);
+        layers['Listed Buildings'] = listedGroup;
+        console.log(layers);
         break;
       
       case 'Conservation Area':
-        createNewLayer(des.geometry, '#FF0000', popup).addTo(map);
+        createNewLayer(des.geometry, colours.consarea, popup).addTo(map);
         break;
 
       case 'Gardens and Designed Landscapes':
-        createNewLayer(des.geometry, '#00FF00', popup).addTo(map);
+        createNewLayer(des.geometry, colours.garden, popup).addTo(map);
         break;
       
       case 'Scheduled Monument':
-        createNewLayer(des.geometry, '#FFFF00', popup).addTo(map);
+        createNewLayer(des.geometry, colours.monument, popup).addTo(map);
         break;
 
       case 'World Heritage Site':
-        createNewLayer(des.geometry, '#0000FF', popup).addTo(map);
+        createNewLayer(des.geometry, colours.world, popup).addTo(map);
         break;
 
       default:
@@ -65,6 +75,8 @@ function getLayers() {
     }
 
   }
+
+  L.control.layers('', layers, {}).addTo(map);
 
 }
 
@@ -78,18 +90,16 @@ function createNewLayer(geometry, fill, popup, type = 'MultiPolygon') {
       pointToLayer: function(point, latlng) {
         
         return new L.CircleMarker(latlng, {
-
           radius: 10,
           color: '#000000',
           weight: 2,
           fillColor: fill,
           fillOpacity: 0.5,
-          
-        }).bindPopup(popup);
+        });
 
       }
 
-    })
+    }).bindPopup(popup);
 
   } else {
 
